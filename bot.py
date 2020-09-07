467208a82c18e8f2f1a92c3c1f4074af13179763
Emre Savaş <e@emresavas.com>
2020-09-08 00:42:30 +0300

    USD to TRY exchange via pa-scraper package

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import discord
 import re
 from bin_checker import get_bin
+from scraper import Scraper
 
 EXISTING_PROMPTAPI_TOKEN = os.environ.get('PROMPTAPI_TOKEN')
 EXISTING_DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
@@ -22,7 +23,7 @@ async def on_message(message):
     if message.content.startswith('!bincheck'):
         os.environ['PROMPTAPI_TOKEN'] = EXISTING_PROMPTAPI_TOKEN
         bin_code_stripped = message.content.strip('!bincheck ')
-        regex = r"\d{6}"
+        regex = r'\d{6}'
         bin_code = re.search(regex, bin_code_stripped)
         if bin_code:
             bin_information = get_bin(int(bin_code.group()))
@@ -34,6 +35,23 @@ async def on_message(message):
                 await message.channel.send(response)
         else:
             await message.channel.send('Please enter the first six digit of a credit card. Ex: 424201')
+            
+    if message.content.startswith('!getusd'):
+        os.environ['PROMPTAPI_TOKEN'] = EXISTING_PROMPTAPI_TOKEN
+        bin_code_stripped = message.content.strip('!getusd ')
+        url = 'https://kur.doviz.com/serbest-piyasa/amerikan-dolari'
+        scraper = Scraper(url)
+        fetch_params = dict(selector='div.col span.value')
+        response = scraper.get(params=fetch_params)
+        if response.get('error', None):
+    	    await message.channel.send(response)
+        else:
+            usd = re.findall(r'[-+]?\d*\,\d+|\d+', f'{response["result"]["data"]}')
+            if usd:
+                await message.channel.send(f'Buying: {usd[0]} - Selling: {usd[1]} Source: {url}')
+            else:
+                await message.channel.send('Site can not scraped or can not find any element with the given css selector.')
+                
 @client.event
 async def on_ready():
     print('Bot Name:')
