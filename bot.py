016757765830e2824d22120e0aaef639bc3b0903
Emre Savaş <e@emresavas.com>
2020-09-07 23:44:50 +0300

    Regex added to strip non-exact match queries

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 # bot.py
 import os
 import discord
+import re
 from bin_checker import get_bin
 
-MY_PROMPTAPI_TOKEN = os.environ.get('PROMPTAPI_TOKEN')
-MY_DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
+EXISTING_PROMPTAPI_TOKEN = os.environ.get('PROMPTAPI_TOKEN')
+EXISTING_DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
 
 client = discord.Client()
 
@@ -19,17 +20,20 @@ async def on_message(message):
         await message.channel.send(msg)
         
     if message.content.startswith('!bincheck'):
-        os.environ['PROMPTAPI_TOKEN'] = MY_PROMPTAPI_TOKEN
-        bin_code_split = message.content.split('!bincheck')
-        bin_code = bin_code_split[1]
-        bin_information = get_bin(int(bin_code))
-        if bin_information.get('error', False):
-            response = f'Error: {bin_information["error"]}'
-            await message.channel.send(response)
+        os.environ['PROMPTAPI_TOKEN'] = EXISTING_PROMPTAPI_TOKEN
+        bin_code_stripped = message.content.strip('!bincheck ')
+        regex = r"\d{6}"
+        bin_code = re.search(regex, bin_code_stripped)
+        if bin_code:
+            bin_information = get_bin(int(bin_code.group()))
+            if bin_information.get('error', False):
+                response = f'Error: {bin_information["error"]}'
+                await message.channel.send(response)
+            else:
+                response = f'Bank name: {bin_information["bank_name"]} - Country: {bin_information["country"]} - URL: {bin_information["url"]} - Type: {bin_information["type"]} - Scheme: {bin_information["scheme"]} - BIN: {bin_information["bin"]}'
+                await message.channel.send(response)
         else:
-            response = f'Bank name: {bin_information["bank_name"]} - Country: {bin_information["country"]} - URL: {bin_information["url"]} - Type: {bin_information["type"]} - Scheme: {bin_information["scheme"]} - BIN: {bin_information["bin"]}'
-            await message.channel.send(response)
-
+            await message.channel.send('Please enter the first six digit of a credit card. Ex: 424201')
 @client.event
 async def on_ready():
     print('Bot Name:')
@@ -37,4 +41,4 @@ async def on_ready():
     print(client.user.id)
     print('Bot started...')
 
-client.run(MY_DISCORD_TOKEN)
+client.run(EXISTING_DISCORD_TOKEN)
